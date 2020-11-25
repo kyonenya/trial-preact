5aeefc8df069c155cd8b9b5f1363a53c5bc20f36
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-25 13:40:37 +0900

    tweak

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-
-// webpack開発用サーバーを設定する
 const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
+const webpackDevMiddleware = require('webpack-dev-middleware');
 const config = require('./webpack.dev.config.js');
-const compiler = webpack(config);
-const middleware = webpackMiddleware(compiler, {
+
+// Setup webpack-dev-middleware
+const middleware = webpackDevMiddleware(webpack(config), {
   publicPath: config.output.publicPath,
-  watchOptions: {
-    // 初回コンパイル後はファイルの変更を監視しない
-    ignored: '**'
-  },
 });
+const app = express();
 app.use(middleware);
 
-// index.htmlにルーティングする
+// Route
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
-// ルートディレクトリを指定する
 const rootDir = __dirname;
 app.use(express.static(rootDir));
 
-// ローカルサーバーを起動する
-const port = process.env['WEB_APP_PORT'];
+// Launch app
+const port = process.env['WEB_APP_PORT']
+  ? process.env['WEB_APP_PORT']
+  : 3000;
 app.listen(port, () => {
   console.log(`Launching app... http://localhost:${port}\n`);
 });
 
-// Register app and middleware. Required for better
-// performance when running from play.js
-try { pjs.register(app, middleware); } catch (error) { }
+// Register app and middleware. Required for better performance when running from play.js
+try {
+  pjs.register(app, middleware);
+} catch (error) {
+  // console.log(error);
+}
