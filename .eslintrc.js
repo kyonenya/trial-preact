40663a72f3fe9558a896a2512a725b2c3022b784
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-25 14:35:00 +0900

    ts向け設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,10 @@ module.exports = {
   },   
   extends: [
     'airbnb-base',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
