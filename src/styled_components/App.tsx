02f650c37f48835986d2366ab24beddcf8c8f34f
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-15 13:44:23 +0900

    s-c grobalStyle

--- src/styled_components/App.tsx
+++ src/styled_components/App.tsx
@@ -1,5 +1,5 @@
 import { h, Fragment } from 'preact';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { Basic } from './Basic';
 import { Extend } from './Extend';
 import { Theme } from './Theme';
@@ -7,9 +7,17 @@ import { Theme } from './Theme';
 export const App = () => {
   return (
     <Fragment>
+      {/* @ts-ignore src/styled_components/App.tsx(11,8): error TS2604: JSX element type 'GlobalStyle' does not have any construct or call signatures. */}
+      <GlobalStyle />
       <Basic isPrimary={true}/>
       <Extend />
       <Theme />
     </Fragment>
   );
 };
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: sans-serif;
+  }
+`;
